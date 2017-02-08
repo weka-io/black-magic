23649f1a03f723935806e517c58243de63d1f493
Idan Arye <idan@weka.io>
2017-02-08 19:10:54 +0200

    `announced_vars` support logging existing variables without assignment

--- python/improved_announced_vars.py
+++ python/improved_announced_vars.py
@@ -2,7 +2,7 @@
 from contextlib import contextmanager
 
 import logging
-logging.basicConfig()
+
 
 class MonkeyPatchLogger(object):
     @contextmanager
@@ -28,20 +28,28 @@ class MonkeyPatchLogger(object):
             yield
             lasti_after = frame.f_lasti
             frame_locals = dict(frame.f_locals)
+            prev_instruction = None
+
+            def log_single_variable(name):
+                try:
+                    value = frame_locals[name]
+                except KeyError:
+                    pass
+                else:
+                    self.info('%s = %s', name, value)
+
             with self.indented(header, *args, footer=False, **kwargs):
                 for instruction in dis.get_instructions(frame.f_code):
                     if instruction.offset <= lasti_before:
                         continue
                     if lasti_after <= instruction.offset:
                         break
-                    if instruction.opname in {'STORE_NAME', 'STORE_FAST'}:
-                        name = instruction.argval
-                        try:
-                            value = frame_locals[name]
-                        except KeyError:
-                            pass
-                        else:
-                            self.info('%s = %s', name, value)
+                    if instruction.opname in {'STORE_NAME', 'STORE_FAST', 'STORE_GLOBAL'}:
+                        log_single_variable(instruction.argval)
+                    elif instruction.opname == 'POP_TOP' and prev_instruction and \
+                            prev_instruction.opname in {'LOAD_NAME', 'LOAD_FAST', 'LOAD_GLOBAL'}:
+                        log_single_variable(prev_instruction.argval)
+                    prev_instruction = instruction
 
         return cm()
 
@@ -49,21 +57,30 @@ class MonkeyPatchLogger(object):
 logging.Logger.__bases__ += (MonkeyPatchLogger,)
 
 
+logging.basicConfig()
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.INFO)
+if __name__ == '__main__':
 
-
-for _ in range(2):
-    with _logger.announced_vars():
-        a = 1
-        b = 2
-
-
-def foo():
+    # Check outside function
     for _ in range(2):
         with _logger.announced_vars():
-            c = 3
-            d = 4
+            a = 1
+            b = 2
 
+    c = 3
+    with _logger.announced_vars():
+        c
 
-foo()
+    # Check inside function
+    def foo():
+        for _ in range(2):
+            with _logger.announced_vars():
+                d = 4
+                e = 5
+
+        f = 6
+        with _logger.announced_vars():
+            f
+
+    foo()
